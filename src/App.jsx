0213f6d1a4235fb70ae674fa61995f598c384b66
KiKiKi <chaika.design@gmail.com>
2021-03-15 23:58:12 +0900

    :books: Booklist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Booklist } from './components/Booklist';
+
 function App() {
   return (
     <div>
       <h1>React APP</h1>
+      <Booklist />
     </div>
   );
 }
---
+++ src/components/Booklist.jsx
@@ -0,0 +1,7 @@
+export const Booklist = () => {
+  return (
+    <div>
+      <p>This. is book list component</p>
+    </div>
+  );
+};
