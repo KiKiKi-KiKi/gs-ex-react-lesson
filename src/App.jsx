537dbf52ec281a0ebe79d49716f410d2f45b70f5
KiKiKi <chaika.design@gmail.com>
2021-03-16 11:57:00 +0900

    :gift: Implement navigation by react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { Booklist } from './components/Booklist';
+
+// React-router-dom
+// https://reactrouter.com/web/api/Route
+
+const Navigation = ({ linkList }) => {
+  const links = linkList.map((link, i) => (
+    <li key={link}>
+      <Link to={i ? `/${link.toLowerCase()}` : '/'}>{link}</Link>
+    </li>
+  ));
+
+  return <ul>{links}</ul>;
+};
 
 function App() {
   const languages = ['React', 'Vue', 'Angular'];
 
   return (
-    <div>
-      <h1>React APP</h1>
-      {languages.map((val) => (
-        <Booklist key={val} language={val} />
-      ))}
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>React APP</h1>
+        <Navigation linkList={languages} />
+        <hr />
+        <Route
+          exact
+          path="/"
+          render={() => <Booklist language={languages[0]} />}
+        />
+        <Route
+          path="/vue"
+          render={() => <Booklist language={languages[1]} />}
+        />
+        <Route
+          path="/angular"
+          render={() => <Booklist language={languages[2]} />}
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
