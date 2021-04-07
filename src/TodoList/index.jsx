52397326481e0f0efb1fc4dbfca97d4d8c044d68
KiKiKi_KiKi <chaika.design@gmail.com>
2021-04-07 14:55:47 +0900

    :circus_tent: Implement show todo list.

--- src/TodoList/index.jsx
+++ src/TodoList/index.jsx
@@ -1,3 +1,60 @@
+import { useState, useEffect } from 'react';
+import { db } from './firebase';
+
+export const COLLECTION = 'todos';
+/*
+TODO item Schema
+interface TodoItem {
+  id: string;
+  todo: string;
+  dueDate: timestamp;
+  createAt: timestamp;
+}
+ */
+
 export const App = () => {
-  return <div>Firebase TODO App</div>;
+  const [todoList, setTodoList] = useState(null);
+
+  const getTodosFromFirebase = async () => {
+    const itemListArray = await db
+      .collection(COLLECTION)
+      .orderBy('createAt')
+      .get();
+
+    const todos = itemListArray.docs.map((item) => {
+      return {
+        id: item.id,
+        data: item.data(),
+      };
+    });
+    setTodoList(todos);
+
+    return todos;
+  };
+
+  useEffect(() => {
+    getTodosFromFirebase();
+  }, []);
+
+  console.log({ todoList });
+
+  return (
+    <div>
+      <h1>Firebase TODO App</h1>
+      <ul>
+        {todoList?.map(({ id, data }) => {
+          return (
+            <li key={id} id={id}>
+              <input type="checkbox" value={id} />
+              <span>{data.todo}</span>
+              <span>DueDate: {data.dueDate.seconds}</span>
+              <button type="button" value={id}>
+                DELETE
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
